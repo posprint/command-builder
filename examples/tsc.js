5ef1327ae1b9517e1c20b77bf697ade512eb9306
Gerow Xu <980378410@qq.com>
2023-02-01 17:38:23 +0800

    tsc section

--- examples/tsc.js
+++ examples/tsc.js
@@ -4,15 +4,15 @@ const escpos = require('escpos')
 escpos.Network = require('escpos-network')
 escpos.USB = require('escpos-usb')
 // escpos.Bluetooth = require('escpos-bluetooth')
-// const device = new escpos.Network('192.168.0.50')
-const device = new escpos.USB(0x1fc9,0x2016)
+const device = new escpos.Network('192.168.9.152')
+// const device = new escpos.USB(0x1fc9,0x2016)
 // const device = new escpos.USB(0x0471, 0x0055)
 
 const {buildCommand, validateNodes} = require('../')
 const testDir = './'
 const data = []
 const exceptions = ['kitchenTscPrint.json']
-const only = ['KitchenTicketTSC2.json']
+const only = ['KitchenTicketTSC.json']
 
 fs.readdirSync(testDir).forEach(file => {
   if (path.extname(file) === '.json') {
@@ -34,7 +34,7 @@ exec = async () => {
     try {
       for (const o of data) {
         current = o
-        const commands = await buildCommand(o, { type: 'TSC', paperSize: [40, 30], encoding: 'GBK', direction: 1, margin:2, space: 3 })
+        const commands = await buildCommand(o, { type: 'TSC', paperSize: [40, 60], encoding: 'GBK', direction: 1, margin:0, space: 3 })
         const buffer = commands.getBuffer().flush()
         printer.raw(buffer)
       }
